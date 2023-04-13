e0c32f52d22f989a9b46e5688754b73c8e75ce0b
DUBlajes <dublajes@gmail.com>
2023-04-13 14:15:20 +0200

    .

--- zzz.tema8.basedatos/src/main/java/main/Main.java
+++ zzz.tema8.basedatos/src/main/java/main/Main.java
@@ -1,9 +1,56 @@
 package main;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import utils.DAO;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		byte opcion = 0;
+		do {
+			System.out.println("¿Qué quieres hacer en la base de datos?\n\t0 - Salir" + "\n\t1 - Insertar usuario"
+					+ "\n\t2 - Borrar usuario" + "\n\t3 - Modificar usuario" + "\n\t4 - Consultar usaurio");
+			opcion = Byte.parseByte(sc.nextLine());
+			switch (opcion) {
+			case 1:
+				try {
+
+					System.out.println("Dime email");
+					String email = sc.nextLine();
+					System.out.println("Dime contraseña");
+					String pass = sc.nextLine();
+					System.out.println("Dime nick");
+					String nick = sc.nextLine();
+
+					DAO.insert("insert into user values('" + email + "','" + pass + "','" + nick + "')");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case 2:
+				try {
+					System.out.println("Dime el email del usuario que quieres borrar");
+					String email = sc.nextLine();
+					int filasBorradas = DAO.delete("delete from user where email='" + email + "'");
+					System.out.println("Se han borrado " + filasBorradas + " filas");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case 3:
+
+				break;
+			case 4:
+
+				break;
+			}
+		} while (opcion != 0);
 
 	}
 
--- zzz.tema8.basedatos/src/main/java/utils/DAO.java
+++ zzz.tema8.basedatos/src/main/java/utils/DAO.java
@@ -10,7 +10,7 @@ public abstract class DAO {
 
 	private static Statement connect() {
 		try {
-			connection = DriverManager.getConnection("jdbc:mysql//localhost:3306/petsdam", "root", "Gv3rr3r0160294!");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/petsdam", "root", "Gv3rr3r0160294!");
 			return connection.createStatement();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
@@ -20,10 +20,28 @@ public abstract class DAO {
 
 	}
 
+	public static int insert(String query) throws SQLException {
+		Statement querier = connect();
+		System.out.println(query);
+		int ret = querier.executeUpdate(query);
+
+		disconnect(querier);
+		return ret;
+	}
+
+	public static int delete(String table) throws SQLException {
+		Statement querier = connect();
+		System.out.println("delete from ");
+		int ret = querier.executeUpdate("delete from "+table+" where ");
+		//TODO MEJORAR CUANDO VEAMOS MAPAS
+		disconnect(querier);
+		return ret;
+	}
+
 	private static void disconnect(Statement smt) {
 		try {
-		smt.close();
-		connection.close();
+			smt.close();
+			connection.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
