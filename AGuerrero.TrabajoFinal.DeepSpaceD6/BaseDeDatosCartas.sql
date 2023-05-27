3cae01c96da6460f46667262380d5be53aab10d3
DUBlajes <dublajes@gmail.com>
2023-05-27 11:20:32 +0200

    Se añaden archivos sql y excel para facilitar entendimiento de la base de datos

--- AGuerrero.TrabajoFinal.DeepSpaceD6/BaseDeDatosCartas.sql
+++ AGuerrero.TrabajoFinal.DeepSpaceD6/BaseDeDatosCartas.sql
@@ -9,8 +9,17 @@ CREATE TABLE Amenazas (
 );
 
 ALTER TABLE Amenazas
-ADD NumeroCartas int,
-ADD DadoDesactivacion int;
+ADD ID int NOT NULL AUTO_INCREMENT PRIMARY KEY;
 
-INSERT INTO Amenazas
-VALUES (value1, value2, value3, ...);
+INSERT INTO Amenazas (Nombre,ValorAmenaza,DadoActivacion,AccionQueRealiza,InternaOExterna,NumeroCartas,DadoDesactivacion)VALUES (
+
+'Alzamiento de los robots',0,2,4,0,1,5);
+
+
+CREATE TABLE AccionQueRealiza (
+	ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Funcion varchar(50)
+);
+
+INSERT INTO AccionQueRealiza
+VALUES ('Ignorar escudos (ataca directamente al casco)','Destruir escudo','Enviar a enfermeria','Tirar dado amenaza','Amenazas externas recuperan 1 nivel','Devolver dado bloqueado','No asignar','No ocurre nada');
